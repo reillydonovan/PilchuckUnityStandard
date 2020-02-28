58a34e6d1e25e2ff00bc6e4a088fa9128f3c2b6f
reillydonovan <reillydonovan3@gmail.com>
2020-02-27 20:35:44 -0800

    added holoplay package

---
+++ Assets/SetLeapHandScale.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Leap.Unity;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SetLeapHandScale : MonoBehaviour
+{
+    public LeapServiceProvider leapController;
+    public bool scaleTheHands = true;
+    public Transform[] hands;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leapController != null)
+        {
+            foreach (Transform hand in hands)
+            {
+                hand.transform.localScale = leapController.transform.localScale;
+            }
+        }
+    }
+}
