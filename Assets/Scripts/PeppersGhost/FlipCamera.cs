6975ab815019c53a5b409f188702908edd768dd0
reillydonovan <reillydonovan3@gmail.com>
2020-02-27 21:47:03 -0800

    added mirror camera support for peppers ghost

---
+++ Assets/Scripts/PeppersGhost/FlipCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Matrix4x4 mat = Camera.main.projectionMatrix;
+        mat *= Matrix4x4.Scale(new Vector3(-1, 1, 1));
+        Camera.main.projectionMatrix = mat;
+        GL.invertCulling = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
