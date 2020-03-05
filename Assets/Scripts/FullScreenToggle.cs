e99b095380329d337b3979c9d4f410caba441757
reillydonovan <reillydonovan3@gmail.com>
2020-03-04 22:48:11 -0800

    added brownian noise to camera for torus

---
+++ Assets/Scripts/FullScreenToggle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullScreenToggle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Screen.fullScreen = true;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.F))
+            Screen.fullScreen = !Screen.fullScreen;
+    }
+}
---
+++ Assets/Scripts/TargetFPS.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFPS : MonoBehaviour
+{
+    public float targetFPS;
+    // Use this for initialization
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 30;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
