4f12703c094966611969b1ec2b2b3a01b91eb43d
reillydonovan <reillydonovan3@gmail.com>
2020-03-05 01:13:04 -0800

    man audio is such a bitch

--- Assets/Scripts/Collisions/DisplacementControl.cs
+++ Assets/Scripts/Collisions/DisplacementControl.cs
@@ -2,13 +2,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+
 
 public class DisplacementControl : MonoBehaviour
 
 
 {
+    //Post Processing properties
 
-   // public GameObject mainCam;
+    Bloom bloomLayer;
+
+    private ProceduralAudioController audioController;
+    public GameObject postProcessVolumeGO;
+    PostProcessVolume postVolume; 
   //  public GameObject audioSource;
   //  public GameObject aura;
    
@@ -26,9 +34,9 @@ public class DisplacementControl : MonoBehaviour
     //  public GameObject aura;
     public float displacementAmount;
     public float shineAmount;
-    public float glowAmount;
-    public float audioAmount;
-    public float audioLerpUp;
+    public float bloomAmount;
+    public float tileAmount;
+   
     public float hueAmount;
     public float blendAmount;
     public float blendDisplacementAmount;
@@ -38,10 +46,18 @@ public class DisplacementControl : MonoBehaviour
     public float timeDivision = 1;
     public float frequencyDisplacementAmount;
     private float rotateDisplacementAmount;
+
+    float xVertexValue;
+
+    public GameObject superShape;
     // public float m1Start;
     // public float m1End;
     // public float m1ChangeAmount;
     // static float t = 0.0f;
+    [Header("Audio Controls")]
+    public float audioAmount;
+    public float audioLerpUp;
+    public float sineWaveAmount, squareWaveAmount, sawWaveAmount, frequencyModulationAmount, frequencyModulationIntentsity;
 
     public ParticleSystem explosionParticles;
     MeshRenderer meshRender;
@@ -53,7 +69,11 @@ public class DisplacementControl : MonoBehaviour
         rotateZ = Time.deltaTime;
         meshRender = GetComponent<MeshRenderer>();
         startTime = Time.time;
-        
+        postVolume = postProcessVolumeGO.GetComponent<PostProcessVolume>();
+        postVolume.profile.TryGetSettings(out bloomLayer);
+
+        audioController = audioSource.GetComponent<ProceduralAudioController>();
+
     }
     private void Update()
     {
@@ -63,27 +83,48 @@ public class DisplacementControl : MonoBehaviour
     private void CollisionController()
     {
         float t = (Time.time - startTime) * speed;
+      
         meshRender.material.color = Color.Lerp(startColor, endColor, t);
-        // t += 0.5f * Time.deltaTime;
-        // m1ChangeAmount = Mathf.Lerp(m1Start, m1End, Time.deltaTime);
-        //   meshRender.material.SetFloat("_Shape1M", m1ChangeAmount);
-        // auraScale = Mathf.Lerp(auraScale, 0, Time.deltaTime);
+
         displacementAmount = Mathf.Lerp(displacementAmount, 0, Time.deltaTime / timeDivision);
-        blendAmount = Mathf.Lerp(blendAmount, 0.1f, Time.deltaTime / timeDivision * 5);
+        blendAmount = Mathf.Lerp(blendAmount, 0.1f, Time.deltaTime / timeDivision);
         shineAmount = Mathf.Lerp(shineAmount, 0, Time.deltaTime);
-        glowAmount = Mathf.Lerp(glowAmount, 0, Time.deltaTime);
+        bloomAmount = Mathf.Lerp(bloomAmount, 0.5f, Time.deltaTime);
         hueAmount = Mathf.Lerp(hueAmount, 0, Time.deltaTime);
-        audioAmount = Mathf.Lerp(audioAmount, .15f, Time.deltaTime / timeDivision);
+        bloomLayer.intensity.value = bloomAmount;
+
+        tileAmount = Mathf.Lerp(tileAmount, 1, Time.deltaTime /(timeDivision * 5));
+
+
+        //Audio Effects
+        audioAmount = Mathf.Lerp(audioAmount, 50f, Time.deltaTime / timeDivision);
         audioLerpUp = Mathf.Lerp(0, audioLerpUp, Time.deltaTime);
-         this.GetComponent<MeshRenderer>().material.SetFloat("_WaveValue1", displacementAmount);
-        //rightHandMesh.GetComponent<SkinnedMeshRenderer>().material.SetFloat("_WaveValue1", displacementAmount);
-        // leftHandMesh.GetComponent<SkinnedMeshRenderer>().material.SetFloat("_WaveValue1", displacementAmount);
+        sawWaveAmount = Mathf.Lerp(sawWaveAmount, 0, Time.deltaTime / timeDivision);
+        sineWaveAmount = Mathf.Lerp(sineWaveAmount, .5f, Time.deltaTime / timeDivision);
+        squareWaveAmount = Mathf.Lerp(squareWaveAmount, 0.1f, Time.deltaTime / timeDivision);
+        frequencyModulationAmount = Mathf.Lerp(frequencyModulationAmount, 0.5f, Time.deltaTime / timeDivision);
+        frequencyModulationIntentsity = Mathf.Lerp(frequencyModulationIntentsity, 3.0f, Time.deltaTime / timeDivision);
+
+        audioController.sinusAudioWaveIntensity = sineWaveAmount;
+        audioController.sawAudioWaveIntensity = sawWaveAmount;
+        audioController.squareAudioWaveIntensity = squareWaveAmount;
+        audioController.amplitudeModulationOscillatorFrequency = audioAmount;
+        audioController.mainFrequency = audioAmount;
+        audioController.frequencyModulationOscillatorFrequency = frequencyModulationAmount;
+        audioController.frequencyModulationOscillatorIntensity = frequencyModulationIntentsity;
+        //audioController.mainFrequency = this.GetComponent<Renderer>().material.GetFloat("_Shape2N1") + 100;
+
+        //Shader Effects
+        this.GetComponent<MeshRenderer>().material.SetFloat("_Blend", blendAmount);
+        this.GetComponent<MeshRenderer>().material.SetFloat("_WaveValue1", displacementAmount);
+        this.GetComponent<MeshRenderer>().material.mainTextureScale = new Vector2(tileAmount, tileAmount);
         meshRender.material.SetFloat("_Emission", shineAmount);
         meshRender.material.SetFloat("_Hue", hueAmount);
+        //rightHandMesh.GetComponent<SkinnedMeshRenderer>().material.SetFloat("_WaveValue1", displacementAmount);
+        // leftHandMesh.GetComponent<SkinnedMeshRenderer>().material.SetFloat("_WaveValue1", displacementAmount);
         //  mainCam.GetComponent<MKGlow>().bloomIntensity = glowAmount;
         //  audioSource.GetComponent<AudioSource>().pitch = audioAmount;
-        audioSource.GetComponent<ProceduralAudioController>().amplitudeModulationOscillatorFrequency = audioAmount;
-        audioSource.GetComponent<ProceduralAudioController>().mainFrequency = audioAmount + 150.0f;
+
 
         stripWidthDisplacementAmount = Mathf.Lerp(stripWidthDisplacementAmount, 1, Time.deltaTime / timeDivision);
         radiusDisplacementAmount = Mathf.Lerp(radiusDisplacementAmount, 10, Time.deltaTime / timeDivision);
@@ -92,7 +133,8 @@ public class DisplacementControl : MonoBehaviour
         rotateDisplacementAmount = Mathf.Lerp(rotateDisplacementAmount, .1f, Time.deltaTime / timeDivision);
         // transformDisplacementAmount = Mathf.Lerp(transformDisplacementAmount, 0, Time.deltaTime / timeDivision);
 
-        this.GetComponent<MeshRenderer>().material.SetFloat("_Blend", blendAmount);
+
+        //RotateObject
         rotateX = rotateDisplacementAmount;
         rotateY = rotateDisplacementAmount;
         rotateZ = rotateDisplacementAmount;
@@ -104,19 +146,7 @@ public class DisplacementControl : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             Debug.Log("Pressed A");
-            // m1ChangeAmount += .1f;
-            displacementAmount += .1f;
-            blendAmount += .1f;
-            shineAmount += .5f;
-            glowAmount += 1f;
-            hueAmount += .3f;
-            audioAmount += 10f;
-            audioLerpUp += 10f;
-            radiusDisplacementAmount += .1f;
-            stripWidthDisplacementAmount += 0.5f;
-            //    explosionParticles.Play();
-            //   auraScale += new Vector3(1f, 0, 0);
-            // Debug.Log("Pressed A");
+            CollisionEvents();
         }
     }
 
@@ -125,20 +155,32 @@ public class DisplacementControl : MonoBehaviour
         //  Debug.Log(collision.collider.name);
         if (collision.collider.tag == "Collision")
         {
-            //   Debug.Log("we hit an obstacle");
-            displacementAmount += .1f;
-            blendAmount += .1f;
-            shineAmount += .5f;
-            glowAmount += 1f;
-            audioAmount += .5f;
-            modulationDisplacmentAmount += .05f;
-            radiusDisplacementAmount += 1.0f / timeDivision;
-            frequencyDisplacementAmount += .1f;
-            stripWidthDisplacementAmount += 0.5f / timeDivision;
-            rotateDisplacementAmount += 0.1f;
-            //  transformDisplacementAmount += .001f / timeDivision;
-            //  explosionParticles.Play();
+            CollisionEvents();
             Debug.Log("On Collision Enter");
         }
+    }
+
+    public void CollisionEvents()
+    {
+        //   Debug.Log("we hit an obstacle");
+        displacementAmount += .1f;
+        blendAmount += .1f;
+        shineAmount += .5f;
+        bloomAmount += 1f;
+        tileAmount += .5f;
+        audioAmount += 10f;
+        modulationDisplacmentAmount += .05f;
+        radiusDisplacementAmount += 1.0f / timeDivision;
+        frequencyDisplacementAmount += .1f;
+        stripWidthDisplacementAmount += 0.5f / timeDivision;
+        rotateDisplacementAmount += 0.1f;
+        frequencyModulationAmount += 0.3f;
+        frequencyModulationIntentsity += 0.5f;
+
+        sineWaveAmount += .1f;
+        squareWaveAmount += .2f;
+        sawWaveAmount += .1f;
+        //  transformDisplacementAmount += .001f / timeDivision;
+        //  explosionParticles.Play();
     }
 }
