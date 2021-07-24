0e5ece89d20bd27f4cbb9c11198b8ba01dffee31
reillydonovan <reillydonovan3@gmail.com>
2021-07-23 19:06:55 -0700

    trying to flip display horizontally

--- Assets/Scripts/PeppersGhost/FlipCamera.cs
+++ Assets/Scripts/PeppersGhost/FlipCamera.cs
@@ -4,20 +4,40 @@ using UnityEngine;
 
 public class FlipCamera : MonoBehaviour
 {
+    public Camera cam;
     // Start is called before the first frame update
-    private Camera thisCamera;
+   // private Camera thisCamera;
     void Start()
-    {
-        thisCamera = GetComponent<Camera>();
-        Matrix4x4 mat = thisCamera.projectionMatrix;
+    {/*  thisCamera = GetComponent<Camera>();
+        Matrix4x4 mat = Camera.main.projectionMatrix;
         mat *= Matrix4x4.Scale(new Vector3(-1, 1, 1));
-        thisCamera.projectionMatrix = mat;
+        Camera.main.projectionMatrix = mat;
         GL.invertCulling = true;
+        */
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnPreCull()
+    {
+        
+            cam.ResetWorldToCameraMatrix();
+            cam.ResetProjectionMatrix();
+        cam.projectionMatrix = cam.projectionMatrix * Matrix4x4.Scale(new Vector3(-1, 1, 1));
+        
+    }
+
+    private void OnPreRender()
+    {
+        GL.invertCulling = true;
+    }
+
+    private void OnPostRender()
+    {
+        GL.invertCulling = false;
     }
 }
